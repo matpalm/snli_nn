8ff044ab8ddf19b0fe3220149e6f1912e9d5e44e
mat kelcey <matthew.kelcey@gmail.com>
2015-10-05 22:06:10 -0700

    add a little MLP pre softmax after concatd final states (include bugfix re: init, Ws were all zero :/)

--- concat_with_softmax.py
+++ concat_with_softmax.py
@@ -1,23 +1,30 @@
+import math
 import theano.tensor as T
 import util
 from updates import vanilla, rmsprop
 
 class ConcatWithSoftmax(object):
-    def __init__(self, states, n_labels, n_hidden):
-        self.states = states
-        self.Wy = util.sharedMatrix(n_labels, len(states) * n_hidden, 'Wy', False)
-        self.by = util.shared(util.zeros((1, n_labels)), 'by')
+    def __init__(self, states, n_labels, n_hidden_previous):
+        self.input = T.concatenate(states)
+        input_size = len(states) * n_hidden_previous
+        # input -> hidden (sized somwhere between size of input & softmax)
+        n_hidden = int(math.sqrt(input_size * n_labels))
+        self.Wih = util.sharedMatrix(input_size, n_hidden, 'Wih')
+        self.bh = util.shared(util.zeros((1, n_hidden)), 'bh')
+        # hidden -> softmax
+        self.Whs = util.sharedMatrix(n_hidden, n_labels, 'Whs')
+        self.bs = util.shared(util.zeros((1, n_labels)), 'bs')
 
     def params(self):
-        return [self.Wy, self.by] 
+        return [self.Wih, self.bh, self.Whs, self.bs]
 
     def updates_wrt_cost(self, cost, learning_rate):
         gradients = T.grad(cost=cost, wrt=self.params())
         return vanilla(self.params(), gradients, learning_rate)
 
     def prob_pred(self):
-        concatted_state = T.concatenate(self.states)
-        prob_y = T.nnet.softmax(T.dot(self.Wy, concatted_state) + self.by)
+        hidden = T.nnet.sigmoid(T.dot(self.input, self.Wih) + self.bh)
+        prob_y = T.nnet.softmax(T.dot(hidden, self.Whs) + self.bs)
         pred_y = T.argmax(prob_y, axis=1)
         return (prob_y, pred_y)
 
